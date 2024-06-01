cb7fcbb7b8d1356c020b17f0b4d05fb6ecaf9411
Joshua <joxuecont@gmail.com>
2024-06-01 08:26:53 -0400

    add users GET endpoint with request file in folder

--- part-04/01-bloglist/controllers/users.js
+++ part-04/01-bloglist/controllers/users.js
@@ -2,6 +2,11 @@ const usersRouter = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
 
