faee0a7cd0efe155e1911a71f2a373044ea66d8f
Joshua <joxuecont@gmail.com>
2024-06-01 17:38:53 -0400

    populate data

--- part-04/01-bloglist/controllers/blogs.js
+++ part-04/01-bloglist/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/Blog');
+const User = require('../models/User');
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -16,9 +17,18 @@ blogRouter.get('/:id', async (request, response, next) => {
 });
 
 blogRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const user = await User.findById(body.userId);
+
+  const blog = new Blog({
+    ...body,
+    user: user.id
+  });
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
