7c217e99422beb550ff7375faada92d7865a6732
Joshua <joxuecont@gmail.com>
2024-06-19 19:37:06 -0400

    build UI to production

--- part-04/01-bloglist-backend/app.js
+++ part-04/01-bloglist-backend/app.js
@@ -14,6 +14,7 @@ connect();
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(express.static('dist'));
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
