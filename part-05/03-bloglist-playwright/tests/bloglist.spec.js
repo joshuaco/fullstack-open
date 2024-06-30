425ada914eae027914fbf23f48af203ab79c710f
Joshua <joxuecont@gmail.com>
2024-06-30 00:26:36 -0400

    5.19-5.21 blog create, update and delete tests

--- part-05/03-bloglist-playwright/tests/bloglist.spec.js
+++ part-05/03-bloglist-playwright/tests/bloglist.spec.js
@@ -1,5 +1,5 @@
 import { test, describe, beforeEach, expect } from '@playwright/test';
-import { loginWith } from './helper';
+import { createBlog, loginWith } from './helper';
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -31,6 +31,64 @@ describe('Blog app', () => {
       await expect(
         page.getByText('Error: username or password incorrect')
       ).toBeVisible();
+    });
+  });
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'test', 'testerization');
+    });
+
+    test('a new blog can be created', async ({ page }) => {
+      createBlog(
+        page,
+        'new blog by playwright',
+        'playwright',
+        'https://www.playwright.dev'
+      );
+
+      await expect(
+        page.getByText(
+          "Blog 'new blog by playwright' has been added successfully!"
+        )
+      ).toBeVisible();
+
+      await expect(
+        page.getByText('new blog by playwright - playwright')
+      ).toBeVisible();
+
+      await expect(page.getByRole('button', { name: 'view' })).toBeVisible();
+    });
+  });
+
+  describe('when blog is created', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'test', 'testerization');
+      await createBlog(
+        page,
+        'another blog from playwright',
+        'playwright',
+        'https://www.playwright.dev'
+      );
+    });
+
+    test('blog can be liked', async ({ page }) => {
+      await page.getByRole('button', { name: 'view' }).click();
+      await page.getByRole('button', { name: 'like' }).click();
+
+      await expect(page.getByText('likes 1')).toBeVisible();
+    });
+
+    test.only('blog can be deleted', async ({ page }) => {
+      // Enable dialog handler
+      page.on('dialog', async (dialog) => await dialog.accept());
+
+      await page.getByRole('button', { name: 'view' }).click();
+      await page.getByRole('button', { name: 'remove' }).click();
+
+      expect(
+        page.getByText('another blog from playwright - playwright')
+      ).not.toBeVisible();
     });
   });
 });
