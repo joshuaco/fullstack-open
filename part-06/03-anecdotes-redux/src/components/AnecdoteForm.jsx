fece69a80513046095ac6bfc3dda9aca47248c6f
Joshua <joxuecont@gmail.com>
2024-07-16 19:39:58 -0400

    6.14-6.15 json-server backend

--- part-06/03-anecdotes-redux/src/components/AnecdoteForm.jsx
+++ part-06/03-anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -1,17 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { create } from '../reducers/anecdoteReducer';
 import { setNotification } from '../reducers/notificationReducer';
+import anecdoteService from '../services/anecdotes';
 
 function AnecdoteForm() {
   const dispatch = useDispatch();
 
-  const submitAnecdote = (e) => {
+  const submitAnecdote = async (e) => {
     e.preventDefault();
 
     const content = e.target.content.value;
     e.target.content.value = '';
 
-    dispatch(create(content));
+    const newAnecdote = await anecdoteService.createNew(content);
+
+    dispatch(create(newAnecdote));
     dispatch(setNotification(`You created '${content}'`));
   };
 
---
+++ part-06/03-anecdotes-redux/src/services/anecdotes.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001/anecdotes';
+
+const asObject = (content) => {
+  return {
+    content,
+    votes: 0
+  };
+};
+
+const getAll = async () => {
+  const response = await axios(baseURL);
+  return response.data;
+};
+
+const createNew = async (content) => {
+  const newObject = asObject(content);
+  const response = await axios.post(baseURL, newObject);
+  return response.data;
+};
+
+export default { getAll, createNew };
