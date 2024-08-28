ccb2dd9fdb04a04b0902b26d7305a01c95ba9fa3
Joshua <joxuecont@gmail.com>
2024-08-27 21:32:18 -0400

    9.11 create PatientWithoutSSN type and get that data

--- part-09/04-patientor-backend/src/services/patientService.ts
+++ part-09/04-patientor-backend/src/services/patientService.ts
@@ -1,8 +1,14 @@
 import patients from '../data/patients';
-import { Patient } from '../types';
+import { PatientWithoutSSN } from '../types';
 
-const getPatients = (): Patient[] => {
-  return patients;
+const getPatients = (): PatientWithoutSSN[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
 };
 
 export default { getPatients };
